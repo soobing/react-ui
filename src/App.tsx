1de82a76a72b6d2001a8a7a4ad6041ae7eae9dfe
soobing <qls0147@naver.com>
2020-08-15 00:19:12 +0900

    refactor: route 처리 (with basename)

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import styled, {
   ThemeProvider,
 } from 'styled-components';
+import {
+  BrowserRouter as Router, Switch, Route, Link,
+} from 'react-router-dom';
 
+import Home from './page/Home';
 import Card from './page/Card';
 import Form from './page/Form';
 
@@ -16,12 +20,54 @@ const Wrapper = styled.div`
   background: ${(props) => props.theme.gray10};
 `;
 
+const Nav = styled.nav`
+  display: flex;
+
+  & > ul {
+    display: flex;
+    flex-direction: row;
+    list-style-type: none;
+    margin-left: auto;
+    margin-right: 10%;
+
+    & > li + li {
+      margin-left: 20px;
+    }
+  }
+`;
+
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <Wrapper>
-        <Card />
-        <Form />
+        <Router basename="/react-ui">
+          <div>
+            <Nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/card">Card</Link>
+                </li>
+                <li>
+                  <Link to="/form">Form</Link>
+                </li>
+              </ul>
+            </Nav>
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/card">
+                <Card />
+              </Route>
+              <Route path="/form">
+                <Form />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
       </Wrapper>
     </ThemeProvider>
   );
